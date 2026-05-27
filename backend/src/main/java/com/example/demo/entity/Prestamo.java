343651c4258cad328f1b68b50bd9cfab5d23f729
MarcosRomaDev <marcos.roma.dev@gmail.com>
2026-05-27 10:38:50 +0200

    Implementado data.sql y schema.sql con create tables e inserts para la base de datos H2. Implementadas entidades libro y prestamo

---
+++ backend/src/main/java/com/example/demo/entity/Prestamo.java
@@ -0,0 +1,85 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "prestamos")
+public class Prestamo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nombreLector;
+
+    @NotNull
+    private LocalDate fechaPrestamo;
+
+    private LocalDate fechaDevolucion;
+
+    @NotBlank
+    private String estado;
+
+    @OneToMany(mappedBy = "prestamo", cascade = CascadeType.ALL)
+    private List<Libro> libros = new ArrayList<Libro>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreLector() {
+        return nombreLector;
+    }
+
+    public void setNombreLector(String nombreLector) {
+        this.nombreLector = nombreLector;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public List<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(List<Libro> libros) {
+        this.libros = libros;
+    }
+}
